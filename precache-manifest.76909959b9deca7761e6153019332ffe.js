0b1420e56ddde6444c46cd500fd470c846fb6259
James Hardy <jamesmhardy33@gmail.com>
2020-07-13 00:24:04 +0100

    Updates

---
+++ precache-manifest.76909959b9deca7761e6153019332ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5443339ce26927429ba8bdf764c192f6",
+    "url": "/game-challenge-bot/index.html"
+  },
+  {
+    "revision": "4c2aad1f70f0ad8808d3",
+    "url": "/game-challenge-bot/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4c2aad1f70f0ad8808d3",
+    "url": "/game-challenge-bot/static/js/2.d9335dfa.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/game-challenge-bot/static/js/2.d9335dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c51971bf47cabb47bd5",
+    "url": "/game-challenge-bot/static/js/main.13cd01d7.chunk.js"
+  },
+  {
+    "revision": "5cb1f9e89e088102a1cf",
+    "url": "/game-challenge-bot/static/js/runtime-main.513bee7f.js"
+  }
+]);
